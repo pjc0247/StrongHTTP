bef5be53cc0ee7784481d2639c105b6e44685b4b
pjc02478 <pjc0247@gmail.com>
2015-11-04 17:38:05 +0900

    ㄹ

--- src/Sample/Google/Maps/MapsAPI.cs
+++ src/Sample/Google/Maps/MapsAPI.cs
@@ -34,40 +34,4 @@ namespace Sample.Google.Maps
             return api;
         }
     }
-
-    [ProcessorTarget(new Type[] { typeof(MapsAPI) })]
-    public class APIKeyProcessor : IParameterProcessor
-    {
-        public void OnParameter(object api, MethodInfo method, List<ParameterData> param)
-        {
-            param.Add(new ParameterData()
-            {
-                type = ParameterType.RequestUri,
-                name = "key",
-                value = ((MapsAPI)api).apiKey
-            });
-        }
-    }
-
-    [ProcessorTarget(new Type[] { typeof(MapsAPI) })]
-    public class LatLngProcessor : IParameterProcessor
-    {
-        public void OnParameter(object api, MethodInfo method, List<ParameterData> param)
-        {
-            if(method.Name == nameof(MapsAPI.ReverseGeocode))
-            {
-                var lat = param.Find(m => m.name == "lat");
-                lat.type = ParameterType.Ignore;
-                var lng = param.Find(m => m.name == "lng");
-                lng.type = ParameterType.Ignore;
-
-                param.Add(new ParameterData()
-                {
-                    type = ParameterType.RequestUri,
-                    name = "latlng",
-                    value = lat.value + "," + lng.value
-                });
-            }
-        }
-    }
 }
