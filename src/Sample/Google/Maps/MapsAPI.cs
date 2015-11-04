0cf785263bf6c9ccaacb448bcc5612823e4f9992
pjc02478 <pjc0247@gmail.com>
2015-11-04 12:31:32 +0900

    샘플 프로젝트 추가

---
+++ src/Sample/Google/Maps/MapsAPI.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CsRestClient;
+using CsRestClient.Attributes;
+
+namespace Sample.Google.Maps
+{
+    [Service("maps/api/geocode")]
+    public interface MapsAPI
+    {
+        [Resource("json")]
+        string ReverseGeocode([RequestUri]string key, [RequestUri]string latlng);
+    }
+
+    public static class MapsAPIFactory
+    {
+        public static MapsAPI Create()
+        {
+            return RemotePoint.Create<MapsAPI>("https://maps.googleapis.com");
+        }
+    }
+}
---
+++ src/Sample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CsRestClient;
+using CsRestClient.Attributes;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var api = Google.Maps.MapsAPIFactory.Create();
+
+            Console.WriteLine(
+                api.ReverseGeocode("", "40.714224,-73.961452"));
+        }
+    }
+}
