17ded43c72701c18bdd17449f0bb44dbd0b4a483
pjc02478 <pjc0247@gmail.com>
2015-11-05 17:59:12 +0900

    hi

--- src/Sample/Google/Maps/Processors.cs
+++ src/Sample/Google/Maps/Processors.cs
@@ -10,6 +10,10 @@ using CsRestClient.Attributes;
 
 namespace Sample.Google.Maps
 {
+    /// <summary>
+    /// MapsAPI의 모든 요청은 API_KEY를 필요로 합니다.
+    /// 이 프로세서는 요청에 key 파라미터를 자동으로 추가합니다.
+    /// </summary>
     [ProcessorTarget(new Type[] { typeof(MapsAPI) })]
     public class APIKeyProcessor : IParameterProcessor
     {
@@ -24,6 +28,13 @@ namespace Sample.Google.Maps
         }
     }
 
+    /// <summary>
+    /// GoogleMapsAPI 스펙은 latlng=11.1111,22.2222 형식으로 좌표를 받지만,
+    /// C# 인터페이스는 double lat, double lng 으로 분리하여 받는게 더 편리합니다.
+    /// 
+    /// 이 프로세서는 lat, lng 처럼 따로받은 파라미터를
+    /// API 스펙에 맞는 latlng 으로 합쳐줍니다.
+    /// </summary>
     [ProcessorTarget(new Type[] { typeof(MapsAPI) })]
     public class LatLngProcessor : IParameterProcessor
     {
