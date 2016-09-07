09e3e94f3455ce2ae937d1629abc5534feb2299a
pjc02478 <pjc0247@gmail.com>
2016-09-07 18:36:24 +0900

    ㄹ

--- src/Sample/Yahoo/YQL.cs
+++ src/Sample/Yahoo/YQL.cs
@@ -13,11 +13,24 @@ namespace Sample.Yahoo
     [Service("v1/public")]
     public interface YQL
     {
-        //https://query.yahooapis.com/v1/public/yql?q=&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys
-            
         [Resource("yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22{0}{1}%22)%3B&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")]
         [JsonPath("query.results.rate.Rate")]
         Task<float> GetCurrencyRate(
             [Binding]string code1, [Binding]string code2);
+
+        [Resource("yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22{0}{1}%22)%3B&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")]
+        [JsonPath("query.results.rate")]
+        Task<CurrencyInfo> GetCurrencyInfo(
+            [Binding]string code1, [Binding]string code2);
+    }
+
+    public class CurrencyInfo
+    {
+        public string Name { get; set; }
+        public float Rate { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime Time { get; set; }
+        public float Ask { get; set; }
+        public float Bid { get; set; }
     }
 }
