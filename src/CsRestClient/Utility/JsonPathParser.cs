a22adcec9f98e63d26ab1a2f57326c0a078379cd
pjc02478 <pjc0247@gmail.com>
2016-09-12 18:33:45 +0900

    f

---
+++ src/CsRestClient/Utility/JsonPathParser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json.Linq;
+
+namespace CsRestClient.Utility
+{
+    public sealed class JsonPathParser
+    {
+        public static T ParseObject<T>(string json, string jsonPath)
+        {
+            JToken jobj = null;
+            jobj = JObject.Parse(json);
+
+            var jtoken = jobj.SelectToken(jsonPath);
+            if (jtoken == null)
+                throw new InvalidOperationException($"JsonPath[{jsonPath}] not avaliable");
+
+            return (T)jtoken.ToObject(typeof(T));
+        }
+        public static T ParseArray<T>(string json, string jsonPath)
+        {
+            JToken jobj = null;
+            jobj = JArray.Parse(json);
+
+            var jtoken = jobj.SelectToken(jsonPath);
+            if (jtoken == null)
+                throw new InvalidOperationException($"JsonPath[{jsonPath}] not avaliable");
+
+            return (T)jtoken.ToObject(typeof(T));
+        }
+    }
+}
--- src/Sample/Yahoo/YQL.cs
+++ src/Sample/Yahoo/YQL.cs
@@ -5,23 +5,48 @@ using System.Text;
 using System.Threading.Tasks;
 
 using CsRestClient;
+using CsRestClient.Utility;
 using CsRestClient.Attributes.Request;
 using CsRestClient.Attributes.Response;
+
+using Newtonsoft.Json;
 
 namespace Sample.Yahoo
 {
     [Service("v1/public")]
-    public interface YQL
+    public interface YQLInterface
     {
-        [Resource("yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22{0}{1}%22)%3B&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")]
-        [JsonPath("query.results.rate.Rate")]
-        Task<float> GetCurrencyRate(
-            [Binding]string code1, [Binding]string code2);
+        [Resource("yql?q={0}&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")]
+        Task<string> Query([Binding]string query);
+    }
 
-        [Resource("yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22{0}{1}%22)%3B&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")]
-        [JsonPath("query.results.rate")]
-        Task<CurrencyInfo> GetCurrencyInfo(
-            [Binding]string code1, [Binding]string code2);
+    public class YQL
+    {
+        private static YQLInterface impl
+        {
+            get
+            {
+                return RemotePoint.Create<YQLInterface>("https://query.yahooapis.com");
+            }
+        }
+
+        public static Task<string> Query(string query)
+        {
+            return impl.Query(query);
+        }
+
+        public static async Task<float> GetCurrencyRate(string code1, string code2)
+        {
+            string json = await impl.Query(
+                $"select * from yahoo.finance.xchange where pair in (\"{code1 + code2}\")");
+            return JsonPathParser.ParseObject<float>(json, "query.results.rate.Rate");
+        }
+        public static async Task<CurrencyInfo> GetCurrencyInfo(string code1, string code2)
+        {
+            string json = await impl.Query(
+                $"select * from yahoo.finance.xchange where pair in (\"{code1 + code2}\")");
+            return JsonPathParser.ParseObject<CurrencyInfo>(json, "query.results.rate");
+        }
     }
 
     public class CurrencyInfo
